e3423535696e4989003eb87041e69929082a0af0
entcheva <christina.entcheva@gmail.com>
2016-12-04 13:35:53 -0500

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,42 +1,17 @@
 class Genre
-
-  attr_reader :name
-
-  #*********************************#
-
-  ALL = []
-
-  def save
-    ALL << self
-  end
-
-  def self.all
-    ALL
-  end
-
-  #*********************************#
+  attr_accessor :name, :songs, :artists
 
   def initialize(name)
-    @name = name # name of genre
-    @songs = [] #songs that are of this genre
+    @name = name
+    @songs = []
   end
 
+  def add_songs(song)
+    @songs << song
+  end
+
+  def artists
+    songs.collect { |song| song.artist }
+  end
 
 end
-
-
-# jay_z = Artist.new("Jay-Z")
-# rap = Genre.new("rap")
-# ninety_nine_problems = Song.new("99 Problems", rap)
-# jay_z.add_song(ninety_nine_problems)
-
-# jay-z = Artist.new
-# #<Artist:0x007fcade03a920>
-
-# rap = Genre.new
-# #<Genre:0x007fcf4122edc8 @name="rap", @songs=[]>
-
-# ninety_nine_problems = Song.new
-# #<Song:0x007fcf4122ec38
-# @genre=#<Genre:0x007fcf4122edc8 @name="rap", @songs=[]>,
-# @name="99 Problems">
--- lib/song.rb
+++ lib/song.rb
@@ -1,47 +1,10 @@
 class Song
-
-  attr_accessor :artist
-  attr_reader :name
-
-  #*********************************#
-
-  ALL = []
-
-  def save
-    ALL << self
-  end
-
-  def self.all
-    ALL
-  end
-
-  #*********************************#
+  attr_accessor :name, :artist, :genre
 
   def initialize(name, genre)
-    @name = name # name of song
-    @genre = genre # genre of song
+    @name = name
+    @genre = genre
+    genre.add_songs(self)
   end
 
-  # tell the song
-
-
-
-
 end
-
-
-# jay_z = Artist.new("Jay-Z")
-# rap = Genre.new("rap")
-# ninety_nine_problems = Song.new("99 Problems", rap)
-# jay_z.add_song(ninety_nine_problems)
-
-# jay-z = Artist.new
-# #<Artist:0x007fcade03a920>
-
-# rap = Genre.new
-# #<Genre:0x007fcf4122edc8 @name="rap", @songs=[]>
-
-# ninety_nine_problems = Song.new
-# #<Song:0x007fcf4122ec38
-# @genre=#<Genre:0x007fcf4122edc8 @name="rap", @songs=[]>,
-# @name="99 Problems">
